d245391a793d8e2c3e79e7bba11a50cad3700954
jeroen1010 <jjaspez92@ieszaidinvergeles.org>
2025-05-20 19:07:51 +0200

    Se me olvidó crear otra pantalla ya además he emtido ya todas las pantallas en la clase screens

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -5,3 +5,7 @@ export 'package:truecinema/screens/registro_screen.dart';
 export 'package:truecinema/screens/recuperar_password_screen.dart';
 export 'package:truecinema/screens/registro_usuario_screen.dart';
 export 'package:truecinema/screens/editar_usuario_screen.dart';
+export 'package:truecinema/screens/busqueda_screen.dart';
+export 'package:truecinema/screens/mi_lista_screen.dart';
+export 'package:truecinema/screens/resenias_screen.dart';
+export 'package:truecinema/screens/mi_perfil_screen.dart';
